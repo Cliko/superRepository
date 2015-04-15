110128b948004896213ce8e2af2673501a3f1d6f
Elodie Barbet <elodie-barbet@cifacom.com>
2015-04-15 14:54:44 +0200

    ajout login dropdown

--- superRepertory.js
+++ superRepertory.js
@@ -18,6 +18,13 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+  // Template.Home = function()
+  // {
+  //     loginButtons.set('dropdownVisible', false);
+  // };
+  Template.Home.rendered = function(){
+    Accounts._loginButtonsSession.set('dropdownVisible', true);
+  };
 }
 
 if (Meteor.isServer) {
