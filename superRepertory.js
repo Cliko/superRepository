c15bfcb0727a4af313ecf3cbb12dc4d2b577b366
Cliko <benjamin-garcia@cifacom.com>
2015-04-15 15:39:44 +0200

    Merge branch 'master' of github.com:Cliko/superRepository
    Conflicts:
    	superRepertory.js

--- superRepertory.js
+++ superRepertory.js
@@ -3,28 +3,11 @@
 
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-  // Template.Home = function()
-  // {
-  //     loginButtons.set('dropdownVisible', false);
-  // };
   Template.Home.rendered = function(){
     Accounts._loginButtonsSession.set('dropdownVisible', true);
   };
+
 }
 
 if (Meteor.isServer) {
